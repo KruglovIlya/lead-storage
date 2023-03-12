aa7960c02546922879f25c113b38b173fca45e6e
KruglovIlya <kruglov-il1@yandex.ru>
2023-03-13 02:20:35 +0400

    add env

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,9 @@
 import { Sequelize } from "sequelize";
 
-const db_path = process.env.DB_PATH;
+// const db_path = process.env.DB_PATH;
 
-const database = new Sequelize("leads", "leads", "leads", {
+
+const database = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   dialect: "mysql",
   host: "localhost",
   port: 3306,
